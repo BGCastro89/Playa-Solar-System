0af6ddaafb2f6862c6b7f1e530b854c351166819
Brian Castro <brian.g.castro@gmail.com>
2019-10-02 21:21:32 -0700

    WIP: added matplotlib polar coord plot, angles seem off

--- planetary_positions/get_planet_positions.py
+++ planetary_positions/get_planet_positions.py
@@ -1,4 +1,8 @@
+# Brian Castro 2019
+
 from astroquery.jplhorizons import Horizons
+import numpy as np
+
 # AstroQuery library info
 # github: https://github.com/astropy/astroquery
 # docs: https://astroquery.readthedocs.io/en/latest/jplhorizons/jplhorizons.html
@@ -29,11 +33,43 @@ def get_planetary_positions(target,type='majorbody'):
     eph['r'].convert_unit_to('km')
     return eph['targetname','datetime_str','EclLon','EclLat','r']
 
-for planet in planets:
-    print "\n",get_planetary_positions(planet)
+# for planet in planets:
+#     print("\n",get_planetary_positions(planet))
 
-for planet in minor_planets:
-    print "\n",get_planetary_positions(planet,"smallbody")
+# for planet in minor_planets:
+#     print("\n",get_planetary_positions(planet,"smallbody"))
+
+def format_planets():
+    planetary_coords = []
+
+    for planet in planets:
+        positions_table = get_planetary_positions(planet)
+        # print(type(planet_pos))
+        # print(planet_pos.colnames)
+        # print(planet_pos.columns)
+        # print(planet_pos[0])
+        data = np.array(positions_table)
+        # print data
+
+        planetary_coords.append(data[0]) # pos of each planet at time 0
+
+    return planetary_coords
+
+
+    # for planet in minor_planets:
+    #     planet_pos = get_planetary_positions(planet,"smallbody")
+
+def get_coords():
+    data = format_planets() # call each separetly?
+    planet_pos_reduced = [(pos[4], pos[2]) for pos in data]
+    theta = [rad[1] for rad in planet_pos_reduced]
+    r = [ang[0] for ang in planet_pos_reduced]
+
+    return (r, theta)
+
+if __name__ == "__main__":
+    data = format_planets() # call each separetly?
+    r, theta = data[4], data[2]
 
 # Future features
 #   Pretty print/file output
