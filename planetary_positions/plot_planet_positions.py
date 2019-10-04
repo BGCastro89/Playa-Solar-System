ff922fb9ba561e5827a1234bfadea28ad727c8a5
Brian Castro <brian.g.castro@gmail.com>
2019-10-03 18:58:05 -0700

    Converted angles for plot input to radians

--- planetary_positions/plot_planet_positions.py
+++ planetary_positions/plot_planet_positions.py
@@ -7,15 +7,14 @@ def debug_pp(r,theta):
         print theta[i], "\t", r[i]
 
 
+def to_rads(angles):
+    return  [np.radians(deg) for deg in angles]
+    
 def generate_plot(r,theta):
-    # r, theta = [1.5,4.,6.,8.,11.], [33.,110.,130.,290.,340.]
 
     debug_pp(r, theta)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='polar')
-    # ax = plt.polar()
-    c = ax.scatter(theta, r)#, c=colors, s=area, cmap='hsv', alpha=0.75)
-    # plt.savefig("solarsys.png")
-    plt.savefig("test.png")
 
-    plt.show()
+    c = ax.scatter(to_rads(theta), r)
+    plt.savefig("solarsys.png")
