f4acd0d2cbc77875da45489d49823754d4f579a9
Brian Castro <brian.g.castro@gmail.com>
2019-10-06 17:18:47 -0700

    fixed polar plot axis and scaled to Billions of Km, added BRC map

--- planetary_positions/plot_planet_positions.py
+++ planetary_positions/plot_planet_positions.py
@@ -4,17 +4,26 @@ import matplotlib.pyplot as plt
 def debug_pp(r,theta):
 
     for i in xrange(0,len(r)):
+        # print int(np.round(r[i]))
         print theta[i], "\t", r[i]
 
 
 def to_rads(angles):
     return  [np.radians(deg) for deg in angles]
-    
+
+def to_Gkm(distances):
+    return  [r/1000000000 for r in distances]
+
 def generate_plot(r,theta):
 
     debug_pp(r, theta)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='polar')
 
-    c = ax.scatter(to_rads(theta), r)
+    r = to_Mkm(r)
+    theta = to_rads(theta)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='polar',xmargin=1.2)
+    ax.set_ylim(0,6)
+
+    c = ax.scatter(theta, r)
     plt.savefig("solarsys.png")
